0770604b1a6acbd2db94ac09a2efbae00674de81
65937 <22028250@myrp.edu.sg>
2023-07-20 15:16:54 +0800

    user class

---
+++ C206_CaseStudy/src/User.java
@@ -0,0 +1,18 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: 65937
+ * Student ID: {type your id here}
+ * Class: {type your class here}
+ * Date/Time created: Thursday 20-07-2023 15:16
+ */
+
+/**
+ * @author 65937
+ *
+ */
+public class User {
+
+}
